8312bc40d38c46b62363a3297731ff9965ac1d95
IK Emmanuel <47215749+Ik-Emmanuel@users.noreply.github.com>
2021-02-21 19:25:58 +0100

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -75,13 +75,18 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+    
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'incomeexpensesdb',
+	    'USER': 'postgres',
+	    'PASSWORD': 'MEMEME2you',
+	    'HOST':'localhost',
+	
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators

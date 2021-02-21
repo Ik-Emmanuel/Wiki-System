f84e3a26d2b9f116fe559c917a027283564f92e4
IK Emmanuel <47215749+Ik-Emmanuel@users.noreply.github.com>
2021-02-21 18:43:43 +0100

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'encyclopedia/static/encyclopedia')]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
+django_heroku.settings(locals())
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
+
